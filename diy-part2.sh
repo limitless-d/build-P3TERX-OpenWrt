527ea1508bf62ca0db25679d9c0d3645a8676824
limitless-d <102198773+limitless-d@users.noreply.github.com>
2026-05-08 00:37:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,39 +27,39 @@ sed -i 's/192.168.1.1/192.168.3.15/g' package/base-files/files/bin/config_genera
 # echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> .config
 # ========== 添加自定义软件包 ==========
 # 方法A：基础软件包
-cat <<EOF >> .config
-CONFIG_TARGET_ROOTFS_PARTSIZE=1024
+# cat <<EOF >> .config
+# CONFIG_TARGET_ROOTFS_PARTSIZE=1024
 
-CONFIG_PACKAGE_ipv6helper=y
-CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
-CONFIG_PACKAGE_odhcpd-ipv6only=y
-CONFIG_PACKAGE_odhcp6c=y
+# CONFIG_PACKAGE_ipv6helper=y
+# CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
+# CONFIG_PACKAGE_odhcpd-ipv6only=y
+# CONFIG_PACKAGE_odhcp6c=y
 
-CONFIG_PACKAGE_kmod-ipv6=y
-CONFIG_PACKAGE_kmod-ip6tables=y
+# CONFIG_PACKAGE_kmod-ipv6=y
+# CONFIG_PACKAGE_kmod-ip6tables=y
 
-CONFIG_PACKAGE_ip6tables=y
+# CONFIG_PACKAGE_ip6tables=y
 
-CONFIG_PACKAGE_luci-proto-ipv6=y
+# CONFIG_PACKAGE_luci-proto-ipv6=y
 
-CONFIG_PACKAGE_luci=y
-CONFIG_PACKAGE_luci-ssl-openssl=y
-CONFIG_PACKAGE_luci-compat=y
-CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
+# CONFIG_PACKAGE_luci=y
+# CONFIG_PACKAGE_luci-ssl-openssl=y
+# CONFIG_PACKAGE_luci-compat=y
+# CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 
-CONFIG_PACKAGE_luci-app-store=y
-CONFIG_PACKAGE_luci-lib-ipkg=y
+# CONFIG_PACKAGE_luci-app-store=y
+# CONFIG_PACKAGE_luci-lib-ipkg=y
 
-CONFIG_PACKAGE_luci-app-openclash=y
+# CONFIG_PACKAGE_luci-app-openclash=y
 
-CONFIG_PACKAGE_luci-app-upnp=y
-CONFIG_PACKAGE_luci-i18n-upnp-zh-cn=y
+# CONFIG_PACKAGE_luci-app-upnp=y
+# CONFIG_PACKAGE_luci-i18n-upnp-zh-cn=y
 
-CONFIG_PACKAGE_kmod-r8169=y
-CONFIG_PACKAGE_kmod-r8125=y
+# CONFIG_PACKAGE_kmod-r8169=y
+# CONFIG_PACKAGE_kmod-r8125=y
 
-CONFIG_PACKAGE_curl=y
+# CONFIG_PACKAGE_curl=y
 
-CONFIG_PACKAGE_nano=y
-CONFIG_PACKAGE_tcpdump=y
-EOF
+# CONFIG_PACKAGE_nano=y
+# CONFIG_PACKAGE_tcpdump=y
+# EOF
